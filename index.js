28927f6fa071e69168459e570d7ae9f5f87147c9
Sadiq.Farah <sfartimyare@yahoo.com>
2022-11-02 15:33:25 -0500

    Added javascript and practiced buttons

---
+++ index.js
@@ -0,0 +1,9 @@
+const inputEl = document.getElementById("input-el")
+
+const submitBtn = document.getElementById("submit-btn")
+
+
+
+submitBtn.addEventListener("click", function() {
+    console.log("buttons been clicked")
+})
